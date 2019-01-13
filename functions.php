1535ba0647658709187fc0c9249b540b24167cc6
Simon Mayerhofer <git@maysi.de>
2019-01-13 22:34:17 +0100

    Add Advanced Custom Fields json functionality

--- functions.php
+++ functions.php
@@ -51,5 +51,8 @@ require_once( 'library/responsive-images.php' );
 /** Gutenberg editor support */
 require_once( 'library/gutenberg.php' );
 
+/** Load and save Advanced Custom Fields JSON files.  */
+require_once( 'library/advanced-custom-fields.php' );
+
 /** If your site requires protocol relative url's for theme assets, uncomment the line below */
 // require_once( 'library/class-foundationpress-protocol-relative-theme-assets.php' );
