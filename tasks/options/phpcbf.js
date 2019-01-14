e33b97ba9a2dc502a6930ba6b62a1f27b0dc02cc
Simon Mayerhofer <git@maysi.de>
2019-01-14 08:24:39 +0100

    Fix phpcbf warnings not autofixed

--- tasks/options/phpcbf.js
+++ tasks/options/phpcbf.js
@@ -5,7 +5,7 @@ module.exports = {
 	options: {
 		// hack: args not directly supported by grunt-phpcbf can be added in the bin path
 		bin:
-			'vendor/bin/phpcbf -p --colors --extensions=php --ignore=.sass-cache,.phpintel,node_modules,vendor --standard=phpcs.ruleset.xml --runtime-set ignore_warnings_on_exit 1',
+			'vendor/bin/phpcbf -p --colors --extensions=php --ignore=.sass-cache,.phpintel,node_modules,vendor --standard=phpcs.ruleset.xml --warning-severity=1',
 		noPatch: false,
 	},
 };
