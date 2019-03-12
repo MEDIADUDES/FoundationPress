9672a5ddd73b14cec3c054e6cc0003293f5f0039
Simon Mayerhofer <git@maysi.de>
2019-03-12 17:30:18 +0100

    Leave canonical URL in the header

--- library/cleanup.php
+++ library/cleanup.php
@@ -53,9 +53,6 @@ if ( ! function_exists( 'foundationpress_cleanup_head' ) ) :
 		// Start link.
 		remove_action( 'wp_head', 'start_post_rel_link', 10 );
 
-		// Canonical.
-		remove_action( 'wp_head', 'rel_canonical', 10 );
-
 		// Shortlink.
 		remove_action( 'wp_head', 'wp_shortlink_wp_head', 10 );
 
