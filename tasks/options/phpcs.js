05cd00ec3790e4f580dd92a0bb34f27d70b985ad
Simon Mayerhofer <git@maysi.de>
2019-03-13 17:38:27 +0100

    Show rule in phpcs output

--- tasks/options/phpcs.js
+++ tasks/options/phpcs.js
@@ -5,7 +5,7 @@ module.exports = {
 	options: {
 		// hack: args not directly supported by grunt-phpcs can be added in the bin path
 		bin:
-			'vendor/bin/phpcs -p --colors --extensions=php --ignore=.sass-cache,.phpintel,node_modules,vendor',
+			'vendor/bin/phpcs -s -p --colors --extensions=php --ignore=.sass-cache,.phpintel,node_modules,vendor',
 		standard: 'phpcs.ruleset.xml',
 	},
 };
