6d48986d1deb10000956da7712bb9ff44de09a8f
Simon Mayerhofer <git@maysi.de>
2019-03-15 21:57:17 +0100

    Fix left sidebar layout

--- page-templates/page-sidebar-left.php
+++ page-templates/page-sidebar-left.php
@@ -8,19 +8,22 @@
 get_header(); ?>
 
 <?php get_template_part( 'template-parts/featured-image' ); ?>
+
 <div class="main-container">
-	<div class="main-grid sidebar-left">
-		<main class="main-content">
+	<div class="main-grid main-grid--sidebar-left">
+		<main class="main-content main-content--with-sidebar">
 			<?php
 			while ( have_posts() ) :
 				the_post();
-				?>
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-				<?php comments_template(); ?>
-			<?php endwhile; ?>
+				get_template_part( 'template-parts/content', 'page' );
+				comments_template();
+			endwhile;
+			?>
 		</main>
-	<?php get_sidebar(); ?>
+
+		<?php get_sidebar(); ?>
 	</div>
 </div>
+
 <?php
 get_footer();
