861c4146e2bf092feb1584414f41d0b9521e4976
Simon Mayerhofer <git@maysi.de>
2019-04-24 16:46:14 +0200

    Include babel polyfills by default

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,17 @@
+/* eslint-disable import/first */
+// set to false if you don't use ES6 syntax which needs polyfills.
+const useES6Polyfills = true;
+
+/* eslint-disable no-underscore-dangle, global-require */
+if (
+	useES6Polyfills &&
+	((typeof window !== 'undefined' && !window._babelPolyfill) ||
+		(typeof global !== 'undefined' && !global._babelPolyfill))
+) {
+	require('@babel/polyfill');
+}
+/* eslint-enable no-underscore-dangle, global-require */
+
 import './lib/foundation';
 
 // because jQuery is included with webpack.ProvidePlugin we can
