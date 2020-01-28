face7c8573423de1dc3d7764b2a6178b92672970
Simon Mayerhofer <git@maysi.de>
2020-01-28 15:34:18 +0100

    Only include babel polyfill if explicitly needed

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,15 +1,13 @@
 /* eslint-disable import/first */
-// set to false if you don't use ES6 syntax which needs polyfills.
-const useES6Polyfills = true;
-
 /* eslint-disable no-underscore-dangle, global-require */
+/* uncomment if babel polyfills are needed.
 if (
-	useES6Polyfills &&
-	((typeof window !== 'undefined' && !window._babelPolyfill) ||
-		(typeof global !== 'undefined' && !global._babelPolyfill))
+	(typeof window !== 'undefined' && !window._babelPolyfill) ||
+	(typeof global !== 'undefined' && !global._babelPolyfill)
 ) {
 	require('@babel/polyfill');
 }
+*/
 /* eslint-enable no-underscore-dangle, global-require */
 
 import './lib/foundation';
