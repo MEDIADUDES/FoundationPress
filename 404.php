0caaa7104ce472d171f6a68fa885576916bcd38d
Simon Mayerhofer <git@maysi.de>
2020-05-26 14:47:01 +0200

    Update 404 page

--- 404.php
+++ 404.php
@@ -13,33 +13,17 @@ get_header(); ?>
 		<main class="main-content main-content--with-sidebar">
 			<article>
 				<header>
-					<h1 class="entry-title"><?php esc_html_e( 'File Not Found', 'foundationpress' ); ?></h1>
+					<h1 class="entry-title"><?php esc_html_e( 'Page not found', 'foundationpress' ); ?></h1>
 				</header>
 
 				<div class="entry-content">
-					<div class="error">
-						<p class="bottom"><?php esc_html_e( 'The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.', 'foundationpress' ); ?></p>
-					</div>
+					<p class="bottom">
+						<?php esc_html_e( 'The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.', 'foundationpress' ); ?>
+					</p>
 
-					<p><?php esc_html_e( 'Please try the following:', 'foundationpress' ); ?></p>
-
-					<ul>
-						<li>
-							<?php esc_html_e( 'Check your spelling', 'foundationpress' ); ?>
-						</li>
-
-						<li>
-							<?php
-							printf(
-								// phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
-								/* translators: %s: home page url */
-								__( 'Return to the <a href="%s">home page</a>', 'foundationpress' ),
-								// phpcs:enable WordPress.Security.EscapeOutput.OutputNotEscaped
-								esc_url( home_url() )
-							);
-							?>
-						</li>
-					</ul>
+					<a class="button" href="<?php echo esc_url( home_url() ); ?>">
+						<?php esc_html_e( 'Return to the homepage', 'foundationpress' ); ?>
+					</a>
 				</div>
 			</article>
 		</main>
