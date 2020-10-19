1b001523017540d6c8390aa900b7099fc09b6a50
Simon Mayerhofer <git@maysi.de>
2020-10-19 15:48:55 +0200

    Add wp_body_open function to header.php

--- header.php
+++ header.php
@@ -17,6 +17,7 @@
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+	<?php wp_body_open(); ?>
 
 	<?php if ( get_theme_mod( 'foundationpress_mobile_menu_layout' ) === 'offcanvas' ) : ?>
 		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
