225f09e52b5f24675990bb3f0a8df2772e93f6e8
Simon Mayerhofer <git@maysi.de>
2021-06-28 11:49:57 +0200

    Fix #9 undefined acf function error

--- inc/blocks/block-accordion.php
+++ inc/blocks/block-accordion.php
@@ -13,23 +13,25 @@ class Block_Accordion extends Block {
 	}
 
 	public static function register_block_type(): void {
-		acf_register_block_type(
-			[
-				'name'            => self::get_name(),
-				'title'           => __( 'Accordion', 'foundationpress' ),
-				'render_template' => 'template-parts/blocks/' . self::get_name() . '.php',
-				'enqueue_style'   => get_template_directory_uri() . '/dist/assets/css/blocks/' . self::get_name() . '.css',
-				'enqueue_script'  => get_template_directory_uri() . '/dist/assets/js/blocks/' . self::get_name() . '.js',
-				'category'        => 'foundationpress',
-				'icon'            => 'editor-help',
-				'keywords'        => [ self::get_name(), 'faq', 'akkordion' ],
-				'supports'        => [
-					'align'      => false,
-					'anchor'     => true,
-					'classNames' => true,
-				],
-			]
-		);
+		if ( function_exists( 'acf_register_block_type' ) ) {
+			acf_register_block_type(
+				[
+					'name'            => self::get_name(),
+					'title'           => __( 'Accordion', 'foundationpress' ),
+					'render_template' => 'template-parts/blocks/' . self::get_name() . '.php',
+					'enqueue_style'   => get_template_directory_uri() . '/dist/assets/css/blocks/' . self::get_name() . '.css',
+					'enqueue_script'  => get_template_directory_uri() . '/dist/assets/js/blocks/' . self::get_name() . '.js',
+					'category'        => 'foundationpress',
+					'icon'            => 'editor-help',
+					'keywords'        => [ self::get_name(), 'faq', 'akkordion' ],
+					'supports'        => [
+						'align'      => false,
+						'anchor'     => true,
+						'classNames' => true,
+					],
+				]
+			);
+		}
 	}
 }
 
